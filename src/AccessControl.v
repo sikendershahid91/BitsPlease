e17b59bc2655ec7659030a563f391d00edfcf765
Sikender Shahid <sikendershahid91@gmail.com>
2018-04-11 10:10:47 -0500

    init access control top '

--- src/AccessControl.v
+++ src/AccessControl.v
@@ -6,8 +6,12 @@
 // with 4 entries in the ROM
 
 module AccessControl(
-
-	); 
+	input clk, 
+	input rst, 
+	input [1:0]  _Request, 
+	input [15:0] _Data_In, 
+	input [0:0]  _Data_In_Load, 
+	output[1:0]  _Status_Frame); 
 // notes
 // if access grant -0 than led redd 
 
