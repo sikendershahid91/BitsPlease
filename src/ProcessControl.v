faab438bd7fd289f8944597b97509606d3c040ae
Sikender Shahid <sikendershahid91@gmail.com>
2018-05-05 23:32:10 -0500

    process control close to finished

--- src/ProcessControl.v
+++ src/ProcessControl.v
@@ -17,7 +17,7 @@ module ProcessControl(
 	//password
 	output reg [15:0] userinput, 
 	output reg [0:0]  load, 
-	output reg [0:0]  password_change,
+//	output reg [0:0]  password_change,
 
 	//lcd & LEDs
 	output reg [2:0] lcd_control,
@@ -29,14 +29,89 @@ module ProcessControl(
 	); 
 
 	parameter 
-			  MAIN=0, 
+			  INIT=0, 
 			  ACCESSCONTROL=1,
-	 		  GAME=2,
-	 		  SCOREBOARD=3; 
+			  TRANSITION=2,
+	 		  GAME=3,
+	 		  SCOREBOARD=4;
+
 	reg [2:0] STATE;
-	reg [0:0] login_flag; // for login and logout 
 
 	always @(posedge clk) begin
-		
+		if(!rst) begin
+			STATE <= INIT; 
+		end
+		else begin
+			case(STATE)
+				INIT: begin
+					buttons_select <= 1; 
+					userinput <= 0; 
+					load <= 0;  
+					lcd_control <= 0; 
+					led_control <= 0; 
+					userid <= 0; 
+					game_score_select <= 0; 
+					if(buttons[0] == 1) begin
+						STATE <= ACCESSCONTROL; 
+					end
+					else begin
+						STATE <= INIT; 
+					end
+				end
+				ACCESSCONTROL: begin // need to add delays
+					buttons_select <= 1; 
+					userinput <= switches[15:0]; 
+					userid <= switches[15:0]; 
+					load <= buttons[0]; 
+					lcd_control <= 1; 
+					if(access_control_fb == 1) begin
+						STATE <= GAME; 
+						led_control <= 2; // green
+					end else begin
+						STATE <= ACCESSCONTROL;
+						led_control <= 1; 
+						lcd_control <= 2;// red
+					end
+				end
+				TRANSITION: begin
+					buttons_select <= 1; 
+					lcd_control <=3; 
+					if(buttons[2] == 1) begin
+						STATE <= GAME; 
+					end
+					else if(buttons[1] == 1) begin
+						STATE <= SCOREBOARD; 
+					end
+					else if(buttons[0] == 1) begin
+						STATE <= INIT; 
+					end
+					else begin
+						STATE <= TRANSITION; 
+					end
+				end
+				GAME: begin
+					buttons_select <= 2; 
+					game_score_select <= 1; 
+					if(game_fb == 1) begin
+						STATE <= TRANSITION; 
+					end
+					else begin
+						STATE <= GAME; 
+					end
+				end
+				SCOREBOARD: begin
+					buttons_select <= 3; 
+					game_score_select <= 2; 
+					if(scoreboard_fb == 1) begin
+						STATE <= TRANSITION; 
+					end else begin
+						STATE <= SCOREBOARD; 
+					end
+				end
+				default: begin
+					STATE <= INIT;
+				end
+			endcase
+		end
 	end
 endmodule 
