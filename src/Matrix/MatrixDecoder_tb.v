87ad4ff4746ad03eee0eb0920dacbb3181913045
Sikender Shahid <sikendershahid91@gmail.com>
2018-05-07 14:45:18 -0500

    matrix test bench ready to be used

--- src/Matrix/MatrixDecoder_tb.v
+++ src/Matrix/MatrixDecoder_tb.v
@@ -5,11 +5,21 @@ module MatrixDecoder_tb();
 	reg [0:0] clk; 
 	reg [7:0] matrix[7:0]; 
 
+
+	/* 
+
+	use this input to test your LED matrix 
+	mondify the delay as needed. 
+
+	{matrix[7],matrix[6],matrix[5],matrix[4],matrix[3],matrix[2],matrix[1],matrix[0]}
+
+	*/
+
 	always begin
 		#10 clk = ~clk; 
 	end
 	initial begin
-		$monitor("[%b]\n[%b]\n[%b]\n[%b]\n[%b]\n[%b]\n[%b]\n[%b]", 
+		$monitor("[%b]\n[%b]\n[%b]\n[%b]\n[%b]\n[%b]\n[%b]\n[%b]\n****************", 
 			matrix[0],
 			matrix[1],
 			matrix[2],
@@ -18,16 +28,35 @@ module MatrixDecoder_tb();
 			matrix[5],
 			matrix[6],
 			matrix[7]); 
-		clk =1; 
+		clk =1;  
+		matrix[0] = 8'b00000000; 
+		matrix[1] = 8'b00000000;
+		matrix[2] = 8'b00000000;
+		matrix[3] = 8'b00000000;
+		matrix[4] = 8'b00000000;
+		matrix[5] = 8'b00000000;
+		matrix[6] = 8'b00000000; 
+		matrix[7] = 8'b00000000;
 
-		#10 @(posedge clk) matrix[0] = 1; 
-		matrix[1] = 1;
-		matrix[2] = 1;
-		matrix[3] = 1;
-		matrix[4] = 1;
-		matrix[5] = 1;
-		matrix[6] = 1; 
-		matrix[7] = 1; 	
+		
+
+		#10 @(posedge clk)  
+		matrix[7] = 8'b00111100;
+		#10 @(posedge clk) 
+		matrix[6] = 8'b00111100; 
+		#10 @(posedge clk) 
+		matrix[5] = 8'b00011100;
+		#10 @(posedge clk) 
+		matrix[4] = 8'b00011000;
+		#10 @(posedge clk) 
+		matrix[3] = 8'b00011000;
+		#10 @(posedge clk) 
+		matrix[2] = 8'b00001000;
+		#10 @(posedge clk)  
+		matrix[1] = 8'b00001000;
+		#10 @(posedge clk) 
+		matrix[0] = 8'b00001000; 
+
 
 	end
 
