0d3f872c7ae8b16474102f60fac632abb94e938a
Sikender Shahid <sikendershahid91@gmail.com>
2018-05-06 20:05:28 -0500

    file structure update and scritp

--- src/copy2sim.sh
+++ src/copy2sim.sh
@@ -3,12 +3,18 @@
 export PATH=$PATH:~/intelFPGA_lite/17.1/modelsim_ase/bin
 
 echo "COPYING THESE FILES OVER TO SIM"
-for files in */*.v; do
+for files in *; do
 	echo $files
-	cp $files ../sim/
+	cp -r $files ../sim/
 done
+
+rm ../sim/copy2sim.sh
 
 # add to your path PATH=$PATH:~/intelFPGA_lite/17.1/modelsim_ase/bin
 
 cd ../sim/
+
+echo "**********  COMPILING FILES IN THE DIRECTORIES"
+vlog */*.v
+echo "**********  COMPILING FILES IN THE FOLDERS"
 vlog *.v
