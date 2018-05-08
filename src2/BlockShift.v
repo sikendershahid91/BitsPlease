bc3e937d72f068f157a58ff1b7407b64000907a4
DennyLuong <denny.luong@outlook.com>
2018-05-08 08:14:13 -0500

    BitShift
    Working

--- src2/BlockShift.v
+++ src2/BlockShift.v
@@ -1,79 +1,79 @@
-module BlockShift(start, blockLoc, stopBtn, adjClkPulse, newBlockLoc, startNext);
-	input start;
-	input [7:0] blockLoc;
-	input stopBtn;
+module BlockShift(startSw, prev, stopBtn, adjClkPulse, newBlockLoc, next);
+	input stopBtn, startSw;
 	input adjClkPulse;
-	output [7:0] newBlockLoc;
-	output reg startNext;
-
+	input [7:0] prev;
+	output wire [7:0] newBlockLoc;
+	output reg next;
 	reg [7:0] tempBlock;
-	reg [7:0] strBlock;
 
 	reg [1:0] state;
 	parameter s_init = 0, s_shift = 1, s_pause = 2;
-	reg enable, direction;
+	reg direction;
 	reg [2:0] xCount;
 
 	always @ (posedge adjClkPulse) begin
-		//start signal to start shifting
-		//possibly a switch
-		//otherwise stay not lit.
-		if(start == 1) begin
-			//when first row
-			// send out signal when stop is pressed for the next inst
-			case(state)
-				s_init: begin
-					if(enable == 1) begin
-						tempBlock <= blockLoc;
-						enable <= 0;
+		if(startSw == 1) begin
+			case (state)
+			s_init: begin
+				tempBlock <= 8'b10000000;
+				direction <= 0;
+				xCount <= 0;
+				state <= s_shift;
+			end
+			s_shift: begin
+				if(stopBtn == 0) begin
+					state <= s_pause;
+				end
+				else begin
+					if(direction == 0) begin
+					xCount <= xCount + 1;
+					tempBlock <= tempBlock>> 1;
+					end
+					else if(direction == 1)begin
+						xCount <= xCount - 1;
+						tempBlock <= tempBlock<< 1;
+						if(tempBlock == 8'b1000000) begin
+							direction <= 0;
+						end
+					end
+					if (xCount == 6)begin
+						direction <= 1;
+					end
+					else if (xCount == 0) begin
+						direction <= 0;
 					end
 					state <= s_shift;
 				end
-				s_shift: begin
-					if (stopBtn == 1) begin
-						state <= s_pause;
-						//Do checks on if there is anything below
+			end
+			s_pause: begin
+				if(prev == 0) begin
+					next <= 1;
+				end
+				else begin
+					if(prev == tempBlock) begin
+						next <= 1;
 					end
 					else begin
-						if(direction == 0) begin
-							xCount <= xCount + 1;
-							tempBlock <= tempBlock >> 1;
-						end
-						else if(direction == 1)begin
-							xCount <= xCount - 1;
-							tempBlock <= tempBlock << 1;
-							if(tempBlock == 8'b1000000) begin
-								direction <= 0;
-							end
-						end
-						if (xCount == 6)begin
-							direction <= 1;
-						end
-						else if (xCount == 0) begin
-							direction <= 0;
-						end
-						state <= s_shift;
+						tempBlock <= 8'b00000000;
+						next <= 0;
 					end
 				end
-				s_pause: begin
-					strBlock <= tempBlock;
-					startNext <= 1;
-					state <= s_pause;
-				end
-				default: begin
-					enable <= 1;
-					xCount <= 0;
-					direction <= 0;
-					state <= s_init;
-				end
-			endcase
+				state <= s_pause;
+			end
+			default: begin
+				tempBlock <= 8'b10000000;
+				direction <= 0;
+				xCount <= 0;
+				next <= 0;
+				state <= s_init;
+			end
+		endcase
 		end
 		else begin
-			tempBlock <= 8'b00000000;
-			startNext <= 0;
+			tempBlock <= 8'b11111111;
+			next <= 0;
 		end
 	end
 
 	assign newBlockLoc = tempBlock;
-
 endmodule
